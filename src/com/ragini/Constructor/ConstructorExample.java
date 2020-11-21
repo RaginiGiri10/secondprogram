af867f74f07266187cfda950b3f99eb49438fb63
RaginiGiri10 <raginigiri10@gmail.com>
2020-11-21 20:59:41 +0530

    comitted

--- src/com/ragini/Constructor/ConstructorExample.java
+++ src/com/ragini/Constructor/ConstructorExample.java
@@ -15,6 +15,7 @@ public class ConstructorExample {
 
 
 	public static void main(String[] args) {
+		System.out.println("Welcome");
 		ConstructorExample constructorExample = new ConstructorExample();
 		
 		
